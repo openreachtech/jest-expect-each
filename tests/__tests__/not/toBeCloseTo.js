2ba9f86de32782e32e2928c5e4324f1ee501ee73
Stew Eucen <stew.eucen@gmail.com>
2022-05-05 00:45:56 +0900

    Add test to expect.each().toBeCloseTo().

---
+++ tests/__tests__/not/toBeCloseTo.js
@@ -0,0 +1,31 @@
+// @ts-check
+'use strict'
+
+require('../../../lib/setup').setup()
+
+describe('expect.each', () => {
+  test('#toBeCloseTo()', () => {
+    const actualTable = [
+      0.1 + 0.2,
+    ]
+    const expectedTable = [
+      0.3,
+    ]
+
+    expect(actualTable[0])
+      .toBe(0.30000000000000004)
+    expect(actualTable[0]).not // <---- !!!!
+      .toBe(expectedTable[0])
+
+    expect(actualTable[0])
+      .toBeCloseTo(expectedTable[0], 15)
+    expect(actualTable[0]).not // <---- !!!!
+      .toBeCloseTo(expectedTable[0], 16)
+
+    // @ts-ignore
+    expect.each(actualTable).not // <---- !!!!
+      .toBeCloseTo(
+        expectedTable.map(it => [it, 16])
+      )
+  })
+})
