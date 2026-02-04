b2da0805b272196e463ba6a34d83d0ad8c8c9ebf
Stew Eucen <stew.eucen@gmail.com>
2026-02-04 17:54:56 +0900

    Rename top level describe() name to 'expect.each()'

--- tests/__tests__/not/toBe.js
+++ tests/__tests__/not/toBe.js
@@ -3,7 +3,7 @@
 require('../../../lib/setup')
   .setup()
 
-describe('expect.each', () => {
+describe('expect.each()', () => {
   test('#toBe()', () => {
     const actualValues = [
       undefined,
